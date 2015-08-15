d8a39963ac05714d7abe3d49629bddddd76fcd6d
CJ Melegrito <cj@mlgrto.com>
2015-08-15 15:48:26 +1000

    new learningz

--- Meteor Basics/simple-todos/simple-todos.js
+++ Meteor Basics/simple-todos/simple-todos.js
@@ -1,23 +1,53 @@
+// This is the "Tasks" mongo database
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  
+  // This is the data that gets passed to the body template
+  Template.body.helpers({
+    tasks: function() {
+      // returns any text from the database, and sorts it
+      return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  
+  Template.body.events({
+    "submit .new-task": function (event) {
+      
+      // prevent browser default
+      event.preventDefault();
+      
+      console.log(event);
+      
+      // get value from the event (the form element)
+      var text = event.target.text.value;
+      
+      // Inserts the input text into the "Tasks" mongo database
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+      
+      // Clear textbox
+      event.target.text.value = "";
+      
     }
+    
+    
   });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  
+  Template.task.events({
+    
+    "click .toggle-checked": function () {
+      // toggles checkstate
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function () {
+      Tasks.remove(this._id);
+    }
+    
   });
+  
 }
