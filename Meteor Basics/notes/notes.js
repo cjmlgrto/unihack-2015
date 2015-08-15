9f4e5751271bfa16eebd7d4192f87d8a24bde2f0
CJ Melegrito <cj@mlgrto.com>
2015-08-15 21:41:19 +1000

    starting to take shape

--- Meteor Basics/notes/notes.js
+++ Meteor Basics/notes/notes.js
@@ -5,11 +5,13 @@ if (Meteor.isClient) {
   Template.body.helpers({
     note_items: function() {
       return Notes.find({}, {sort: {modified: -1}});
+    },
+    open_notes: function() {
+      return Notes.find({open: true});
     }
   });
   
   Template.body.events({
-    
     // create-note
     "click #create-note": function() {
       var d = new Date();
@@ -28,25 +30,30 @@ if (Meteor.isClient) {
       Notes.remove(this._id);
     },
     // open the note
-    "click .open": function() {
+    "click .note_item": function() {
+      // update only the clicked one
       Notes.update(
         this._id,
         {
           $set: {open: ! this.open}
         }
       );
-    },
-    // edit the note
+    }    
+  });
+  
+  Template.open_note.events({
     "change .editor": function() {
       var id = this._id + "-open"
       var text = document.getElementById(id).value
       var d = new Date();
       var date = d.toUTCString();
+      
+      console.log(text);
+      
       Notes.update(this._id, {
         text: text,
         modified: date
       });
-      
     }
   });
   
