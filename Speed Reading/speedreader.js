acb504525c60bc39e6c55b36080ff05a1deade6d
hanskek <hanskek0309@gmail.com>
2015-08-16 07:25:11 +1000

    more things

--- Speed Reading/speedreader.js
+++ Speed Reading/speedreader.js
@@ -1,15 +1,15 @@
 function strip(text){
 	words_f=[]
 	var chars=['"',"'",'/','(',')',',','.',';',':','!','#','$','*','?'];
-	for(i=0, i<text.length, i++){
+	for(i=0; i<text.length; i++){
 		for(n=0, n<chars.length, n++){
 			if(text[i]==chars[n]){
-				text=text.replace(i, ' ');
+				text=text.replace(i,' ');
 			}
 		}
 	}
 	var words=text.split(' ');
-	for(i=0, i<words.length, i++){
+	for(i=0; i<words.length; i++){
 		if(words[i]!=''){
 			words_f.push(words[i]);
 		}
