e42f587913b5e4456f6854f0c9e00ed39a9e13df
CJ Melegrito <cj@mlgrto.com>
2015-08-15 20:16:45 +1000

    got notes working m8 yeeeeeeeeeeeeee
    #fiftyshadesofhans

--- Meteor Basics/notes/notes.js
+++ Meteor Basics/notes/notes.js
@@ -3,26 +3,52 @@ Notes = new Mongo.Collection("notes");
 if (Meteor.isClient) {
   
   Template.body.helpers({
-    notes: function () {
-      return Notes.find({}, {sort: {created: -1}});
+    note_items: function() {
+      return Notes.find({}, {sort: {modified: -1}});
     }
   });
   
   Template.body.events({
-    "click .create-new-note": function() {
-      var text = document.getElementById('new-note').value;
+    
+    // create-note
+    "click #create-note": function() {
+      var d = new Date();
+      var date = d.toUTCString();
       Notes.insert({
-        text: text,
-        created: new Date()
+        text: "",
+        modified: date
       });
-      text = "";
     }
+    
   });
   
-  Template.note.events({
-    "click .edit-note": function() {
+  Template.note_item.events({
+    // deletes the note
+    "click .delete": function() {
+      Notes.remove(this._id);
+    },
+    // open the note
+    "click .open": function() {
+      Notes.update(
+        this._id,
+        {
+          $set: {open: ! this.open}
+        }
+      );
+    },
+    // edit the note
+    "change .editor": function() {
+      var id = this._id + "-open"
+      var text = document.getElementById(id).value
+      var d = new Date();
+      var date = d.toUTCString();
+      Notes.update(this._id, {
+        text: text,
+        modified: date
+      });
       
     }
   });
   
-}
+  
+};
